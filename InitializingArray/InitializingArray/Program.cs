f3abdd040918e2629b970e4f703c9acc45a506ac
leesungeun07 <63436892+leesungeun07@users.noreply.github.com>
2020-08-24 22:10:16 +0900

    8월 24일

---
+++ InitializingArray/InitializingArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InitializingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string array1 = new string[3] { "안녕", "Hello", "Halo" };
+            //Console.WriteLine("array1...");
+            //foreach (string greeting in array1)
+            //    Console.WriteLine($"{greeting}");
+
+            string[] array2 = new string[] { "안녕", "Hello", "Halo" };
+            Console.WriteLine("\narray2...");
+            foreach (string greeting in array2)
+                Console.WriteLine($"{greeting}");
+
+            string[] array3 = new string[] { "안녕", "Hello", "Halo" };
+            foreach (string greeting in array3)
+                Console.WriteLine($"{greeting}");
+        }
+    }
+}
