90f5b01d4e1d5790f9fa41380ef17180aa415197
leesungeun07 <63436892+leesungeun07@users.noreply.github.com>
2020-09-09 17:41:59 +0900

    9월 9일

---
+++ SimpleWindow/SimpleWindow/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWindow
+{
+    class Program : Form
+    {
+        static void Main(string[] args)
+        {
+            Application.Run(new Program());
+        }
+    }
+}
---
+++ UsingControls/UsingControls/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingControls
+{
+    public partial class MainForm : Form
+    {
+        Random random = new Random(37);
+        public MainForm()
+        {
+            InitializeComponent();
+
+            lvDummy.Columns.Add("Name");
+            lvDummy.Columns.Add("Depth");
+        }
+        void TreeToList()
+        {
+            lvDummy.Items.Clear();
+            foreach (TreeNode node in tvDummy.Nodes)
+                TreeToList(node);
+        }
+        void TreeToList(TreeNode Node)
+        {
+            lvDummy.Items.Add(new ListViewItem(new string[] { Node.Text, Node.FullPath.Count(f => f=='\\').ToString()}));
+            foreach(TreeNode node in Node.Nodes)
+            {
+                TreeToList(node);
+            }
+        }
+
+        private void GroupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GroupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        void ChangeFont()
+        {
+            if (cboFont.SelectedIndex < 0) return;
+            FontStyle style = FontStyle.Regular;
+            if (chkBold.Checked)
+                style |= FontStyle.Bold;
+            if (chkItalic.Checked)
+                style |= FontStyle.Italic;
+
+            txtSampleText.Font = new Font((string)cboFont.SelectedItem, 10, style);
+        }
+
+        private void CboFont_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
+        }
+        private void ChkBold_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
+        }
+        private void ChkItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
+        }
+
+        private void TbDummy_Scroll(object sender, EventArgs e)
+        {
+            pgDummy.Value = tbDummy.Value;
+        }
+
+        private void BtnModal_Click(object sender, EventArgs e)
+        {
+            Form frm = new Form();
+            frm.Text = "Modal Form";
+            frm.Width = 300;
+            frm.Height = 100;
+            frm.BackColor = Color.Red;
+            frm.ShowDialog();
+        }
+
+        private void BtnMsgBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtSampleText.Text, "Test", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void BtnAddRoot_Click(object sender, EventArgs e)
+        {
+            tvDummy.Nodes.Add(random.Next().ToString());
+            TreeToList();
+        }
+
+        private void BtnAddChild_Click(object sender, EventArgs e)
+        {
+            if(tvDummy.SelectedNode == null)
+            {
+                MessageBox.Show("선택된 노드가 없습니다.", "Treeview Test", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            tvDummy.SelectedNode.Nodes.Add(random.Next().ToString());
+            tvDummy.SelectedNode.Expand();
+            TreeToList();
+        }
+    }
+}
