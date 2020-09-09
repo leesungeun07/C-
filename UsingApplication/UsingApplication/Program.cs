75c7f1cd33dea5e64d99b2c3b500d5361f938df5
leesungeun07 <63436892+leesungeun07@users.noreply.github.com>
2020-09-09 17:42:19 +0900

    9월 9일

---
+++ UsingApplication/UsingApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingApplication
+{
+    class Program : Form
+    {
+        static void Main(string[] args)
+        {
+            Program form = new Program();
+            form.Click += new EventHandler(
+                (sender, eventArgs) => {
+                    Console.WriteLine("Closing Window");
+                    Application.Exit();
+                }
+                );
+            Console.WriteLine("Starting...");
+            Application.Run(form);
+            Console.WriteLine("Exiting..");
+        }
+    }
+}
